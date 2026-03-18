8c338a559a3b17f80a91306f596dd9f15e79a725
utopiasz <s3ndyy3@gmail.com>
2026-03-18 09:33:13 +0100

    working 'space' to stop/play video

--- src/ReelsVideoEditor.App/Views/MainWindow.axaml.cs
+++ src/ReelsVideoEditor.App/Views/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using ReelsVideoEditor.App.ViewModels;
 
 namespace ReelsVideoEditor.App.Views;
 
@@ -7,5 +10,27 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        AddHandler(KeyDownEvent, OnWindowKeyDown, RoutingStrategies.Tunnel);
+    }
+
+    private void OnWindowKeyDown(object? sender, KeyEventArgs eventArgs)
+    {
+        if (eventArgs.Key != Key.Space)
+        {
+            return;
+        }
+
+        if (DataContext is not MainWindowViewModel viewModel)
+        {
+            return;
+        }
+
+        var command = viewModel.Preview.TogglePlayPauseCommand;
+        if (command.CanExecute(null))
+        {
+            command.Execute(null);
+        }
+
+        eventArgs.Handled = true;
     }
 }
